74f773daa1e8a6c6ff867aad7b8a9f84a946bcae
Timothy Osborne <tosborne1215@gmail.com>
2021-11-26 22:50:59 -0800

    basically added everything

--- src/main/events/interactionCreate.js
+++ src/main/events/interactionCreate.js
@@ -1,11 +1,10 @@
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 		if (!interaction.isCommand()) return;
+		console.log("command interaction");
 
 		const command = interaction.client.commands.get(interaction.commandName);
-
 		if (!command) return;
 
 		try {
