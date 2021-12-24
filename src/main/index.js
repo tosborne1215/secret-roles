7b4e4a69bbc443e665e910e999d826d684d06f9e
Timothy Osborne <tosborne1215@gmail.com>
2021-12-24 12:48:39 -0800

    Switched from config to .env and updated the deploy command to be global Now using docker

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('../../config.json');
+require('dotenv').config()
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -28,4 +28,4 @@ for (const file of eventFiles) {
 }
 
 // Login to Discord with your client's token
-client.login(token);
+client.login(process.env.TOKEN);
